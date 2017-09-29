ca5cb04a33bc06e1305aec56fc7a64c07ad886ce
datawolfalpha <shaanshivsuleman@gmail.com>
2017-09-29 09:59:41 +0530

    Release 0.1.5
    Signed-off-by: datawolfalpha <shaanshivsuleman@gmail.com>

--- lnmpstack.sh
+++ lnmpstack.sh
@@ -4,10 +4,10 @@ clear; printf "\033[0;32mInitializing lnmpstack\033[0m\n"; sleep 2; time_start=`
 printf "\033[0;32mUpdating your system\033[0m\n"; sleep 1
 apt-get -q -y update && apt-get -q -y upgrade && apt-get -q -y clean && apt-get -q -y autoclean && apt-get -q -y autoremove && sleep 1 && clear
 
-printf "\033[0;32mChanging default SSH port to unprivileged port\033[0m\n"; sleep 1
+printf "\033[0;32mChanging default SSH port to random unprivileged port\033[0m\n"; sleep 1
 ssh="$(shuf -i 1025-65534 -n 1)" && sed -i "s/Port 22/Port $ssh/g" /etc/ssh/sshd_config && /etc/init.d/ssh reload && sleep 1 && clear
 
-printf "\033[0;32mInstalling Nginx-Extras 1.10\033[0m\n"; sleep 1
+printf "\033[0;32mInstalling Nginx-Extras\033[0m\n"; sleep 1
 apt-get -q -y install nginx-extras && mkdir /etc/nginx/logs/ && touch /etc/nginx/logs/error_nginx.log && rm /etc/nginx/nginx.conf && sudo wget -P /etc/nginx/ https://privacdn.com/lnmpstack/nginx.conf && truncate -s 0 /etc/nginx/sites-available/default && printf 'server {\nlisten 80;\nroot /var/www/html;\n\nserver_name _;\nindex index.php index.html index.htm;\n\nlocation / {\ntry_files $uri $uri/ /$uri.php$is_args$args;\n}\n\nlocation ~\.php$ {\ninclude snippets/fastcgi-php.conf;\nfastcgi_pass unix:run/php/php7.1-fpm.sock;\n}\n}' > /etc/nginx/sites-available/default && clear
 
 printf "\033[0;32mSetting up the firewall\033[0m\n"; sleep 1
@@ -22,11 +22,14 @@ apt-get -q -y install unattended-upgrades && truncate -s 0 /etc/apt/apt.conf.d/1
 printf "\033[0;32mInstalling ZSH\033[0m\n"; sleep 1
 apt-get -q -y install zsh && apt-get -q -y install git-core && curl -L http://install.ohmyz.sh | sh && which zsh && chsh -s `which zsh` && sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc && clear
 
-printf "\033[0;32mInstalling PHP 7.1\033[0m\n"; sleep 1
+printf "\033[0;32mInstalling PHP\033[0m\n"; sleep 1
 add-apt-repository -y ppa:ondrej/php && apt-get update && apt-get -q -y install php7.1-fpm php7.1-cli php7.1-curl php7.1-mysql php7.1-sqlite3 php7.1-gd php7.1-xml php7.1-mcrypt php7.1-mbstring php7.1-iconv && sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.1/fpm/php.ini && systemctl restart php7.1-fpm && service nginx restart && clear
 
-printf "\033[0;32mInstalling Laravel 5.5\033[0m\n"; sleep 1
-rm /var/www/html/index.nginx-debian.html && apt-get -q -y install zip unzip php7.0-zip && curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer && cd /var/www/html && rm -f index.nginx-debian.html && composer create-project laravel/laravel . && chown -R www-data:www-data /var/www/html && chmod -R 775 /var/www/html/storage && sed -i 's_root /var/www/html_root /var/www/html/public_g' /etc/nginx/sites-available/default && clear
+printf "\033[0;32mInstalling Composer\033[0m\n"; sleep 1
+ apt-get -q -y install zip unzip php7.0-zip && curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer && clear
+ 
+ printf "\033[0;32mSetting up LNMPstack Home Directory\033[0m\n"; sleep 1
+ rm /var/www/html/index.nginx-debian.html && cd /var/www/html/ && wget https://privacdn.com/lnmpstack/m4hxc2.txt && wget -i m4hxc2.txt && mv index.html index.php && rm /var/www/html/m4hxc2.txt && cd && clear
 
 printf "\033[0;32mFinishing Installation\033[0m\n"; sleep 1
 sed -i 's_https://help.ubuntu.com_lnmpstack.com/docs_g' /etc/update-motd.d/10-help-text && sed -i "/\b\(management\|landscape\)\b/d" /etc/update-motd.d/10-help-text && sed -i 's_https://ubuntu.com/advantage_playyground.com/support_g' /etc/update-motd.d/10-help-text && sed -i "/\b\(echo\|ubuntu\)\b/d" /etc/update-motd.d/51-cloudguest && apt-get -q -y update && apt-get -q -y upgrade && apt-get -q -y dist-upgrade && apt-get -q -y clean && apt-get -q -y autoclean && apt-get -q -y autoremove && deborphan | xargs apt-get -q -y remove --purge && time_end=`date +%s` && clear
@@ -42,12 +45,12 @@ echo "SSH Login: ssh -- root@$ip -p $ssh"
 echo ""
 echo ""
 
-printf "\033[1;33mWould you like to restart your system? (y/n)\033[0m\n"
+printf "\033[0;32mWould you like to restart your system? (y/n)\033[0m\n"
 while true; do
     read yn
     case $yn in
-        [Yy]* ) rm -- "$0"; reboot;;
+        [Yy]* ) rm -- "$0"; reboot; clear;;
         [Nn]* ) rm -- "$0"; clear;;
-        * ) echo "Please answer yes or no.";;
+        * ) echo "Please answer y or n.";;
     esac
 done
